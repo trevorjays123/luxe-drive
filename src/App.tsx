33438cb647ceaff417ae319ab4aea577e1791ef0
Your Name <trevorjenkins240@gmail.com>
2026-04-27 11:04:56 +0100

    fix: added basename slash

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/luxe-drive">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/fleet" element={<Fleet />} />
