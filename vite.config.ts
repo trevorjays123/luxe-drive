0a1e677329a33dd5995bcd2f64b2dc8027d8cfa5
Your Name <trevorjenkins240@gmail.com>
2026-04-27 02:55:43 +0100

    "Updated base path"

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: '/luxe-drive/', 
     host: "::",
     port: 8080,
     hmr: {
